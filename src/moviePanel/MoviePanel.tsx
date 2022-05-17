585b7d91f33ce2b3cd42ef84406cf4b7b7956dcd
Pat <pat.matheson@gmail.com>
2022-05-16 20:16:21 -0600

    drang and drop working, adding IMDB Regex and making button do things

--- src/moviePanel/MoviePanel.tsx
+++ src/moviePanel/MoviePanel.tsx
@@ -3,15 +3,45 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Container, TextField, Tooltip, Typography } from '@mui/material';
 
+
+
 export default function MoviePanel() {
+
+  const [imdburl, setimdburl] = React.useState('');
+
+  const imdbChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setimdburl(event.target.value);
+  };
+
+  const addIMDBMovie = (url: string) => {
+    console.log(`Imdb Link is: ${url}`);
+    const re = /^https:\/\/www\.imdb\.com\/title\/(tt\d+)\/?/;
+    const match = url.match(re);
+    console.log(`Regex match: ${match}`);
+    if (!match || match.length != 2) {
+      alert("Invaild IMDB url.");
+      return;
+    }
+    const imdbId = match[1];
+  }
+
   return (
     <Stack spacing={2} >
       <Typography textAlign="center" variant="h5" component="div">
         Movie Nominations
       </Typography>
       <Stack  direction="row" justifyContent = "space-between" spacing={2}>
-        <TextField sx={{ maxWidth:"70%"}} id="movieLink" label="IMDB Link" variant="outlined" />
-        <Button variant="contained" >Submit</Button>
+        <TextField sx={{ maxWidth:"70%"}} 
+          id="movieLink" 
+          label="IMDB Link" 
+          variant="outlined"
+          onChange={imdbChange}
+        />
+        <Button 
+          variant="contained" 
+          onClick={() => addIMDBMovie(imdburl)}>
+          Submit
+        </Button>
       </Stack>
     </Stack>
   );
--- src/votingPanel/VotingList.tsx
+++ src/votingPanel/VotingList.tsx
@@ -2,9 +2,10 @@ import { IconButton, List, ListItem } from '@mui/material';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import ListItemText from '@mui/material/ListItemText';
+import react, { useState } from 'react';
 
 export default function VotingList() {
-  const movies = [
+  const defaultMovies = [
     {
       id: "gangs",
       name: 'Gangs of New York'
@@ -15,25 +16,34 @@ export default function VotingList() {
     }
   ];
 
+  const [movies, setMovies] = useState(defaultMovies);
 
+  const DragEnd = (result: any) =>{
+    console.log (result);
+    const items = Array.from(movies);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
 
-  const tempDragEnd = () =>{
-
+    setMovies(items);
   }
 
+  
 
   return (
-    <DragDropContext onDragEnd={tempDragEnd}>
-      <Droppable droppableId='movies'>
+    <DragDropContext onDragEnd={DragEnd}>
+      <Droppable droppableId="movies">
         {(provided) => (
           <List className="movies" {...provided.droppableProps} ref={provided.innerRef}>
             {movies.map(({id, name}, index) => {
               return (
                 <Draggable key={id} draggableId={id} index={index}>
                   {(provided) => (
-                    <ListItem  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <ListItem  
+                      ref= {provided.innerRef} 
+                      {...provided.draggableProps} 
+                      >
                       <ListItemText primary={ name } />
-                      <IconButton edge="end">
+                      <IconButton edge="end" {...provided.dragHandleProps}>
                           <DragHandleIcon />
                       </IconButton>
                     </ListItem>
@@ -41,9 +51,9 @@ export default function VotingList() {
                 </Draggable>                      
               );
             })}
-            
+            {provided.placeholder}
           </List>
-        )}
+      )}
       </Droppable>
     </DragDropContext>
   );
